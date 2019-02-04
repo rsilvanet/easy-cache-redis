89b082ddb8325c28f0971d22b2f52590a00755b4
Robson <me@rsilva.net>
2019-02-03 23:39:59 -0200

    Base project and storage class created

---
+++ src/EasyCache.NET.Redis/RedisStorage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using EasyCache.NET.Storage;
+
+namespace EasyCache.NET.Redis
+{
+    public class RedisStorage : ICacheStorage
+    {
+        public T GetValue<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetValue<T>(string key, T value, TimeSpan expiration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsValidKey(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveKey(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
